2060e72966493016aab0ab55404dbaaa84f59256
Rakha Yudhistira <91657887+rakhayudhis@users.noreply.github.com>
2024-10-20 23:54:51 +0700

    Add files via upload

---
+++ 2311102010_Rakha Yudhistira_Modul_4/guided2.go
@@ -0,0 +1,15 @@
+package main
+import "fmt"
+
+func sendEmailNotification(email string)  {
+	fmt.Printf("Mengirim email ke %s: Pendaftaran berhasil.\n", email)
+}
+
+func main()  {
+	emails := []string{"user@example.com", "user2@example.com", "user3@example.com"}
+
+	fmt.Println("Mengirim email ke pengguna yang baru terdaftar:")
+	for _, email := range emails {
+		sendEmailNotification(email)
+	}
+}
---
+++ 2311102010_Rakha Yudhistira_Modul_4/unguided2.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func hitungSkor(jumlahSoal *int, totalSkor *int, waktuPeserta []int) {
+	*jumlahSoal = 0
+	*totalSkor = 0
+	for _, waktu := range waktuPeserta {
+		if waktu <= 300 {
+			*totalSkor += waktu
+			*jumlahSoal += 1
+		}
+	}
+}
+func main() {
+	var namaPeserta string
+	var waktuPeserta []int
+	var pemenang string
+	var maxSoal, minWaktu int
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, _ := reader.ReadString('\n')
+		line = strings.TrimSpace(line)
+		if line == "Selesai" {
+			break
+		}
+		input := strings.Fields(line)
+		namaPeserta = input[0]
+		waktuPeserta = make([]int, 0)
+		for _, w := range input[1:] {
+			waktu, err := strconv.Atoi(w)
+			if err == nil {
+				waktuPeserta = append(waktuPeserta, waktu)
+			}
+		}
+		var jumlahSoal, totalSkor int
+		hitungSkor(&jumlahSoal, &totalSkor, waktuPeserta)
+		if jumlahSoal > maxSoal || (jumlahSoal == maxSoal &&
+			totalSkor < minWaktu) {
+			pemenang = namaPeserta
+			maxSoal = jumlahSoal
+			minWaktu = totalSkor
+		}
+	}
+	if pemenang != "" {
+		fmt.Printf("%s %d %d\n", pemenang, maxSoal, minWaktu)
+	} else {
+		fmt.Println("Tidak ada peserta.")
+	}
+}
