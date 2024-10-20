438f0ddd96a669ca7dc2a50ba8ee41cd5fe1cfb0
MIhabAufaRafi <2311102226@ittelkom-pwt.ac.id>
2024-10-20 15:15:05 +0700

    Create Guided4-3.go

---
+++ 2311102226_Muhammad Ihab Aufa Rafi/Guided4-3.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func f1(x, y int) float64{
+	var hasil float64
+	hasil = float64(2*x) - 0.5*float64(y) + 3.0
+	return hasil
+}
+
+func f2(x, y int, hasil *float64) { //pass by reference
+	*hasil = float64(2*x) - 0.5*float64(y) + 3.0 //pass by value
+}
+
+func main(){
+	var a, b int
+	var c float64
+
+	//take input for a and b
+	fmt.Print("Enter two integers: ")
+	fmt.Scan(&a, &b)
+
+	//call f2 to calculate and store the result in c
+	f2(a, b, &c)
+
+	//Print the result from f2
+	fmt.Println("Result from f2 (stored in c): ", c)
+
+	//call f1 and print the result
+	resultF1 := f1(b, a) //(6 4)
+	fmt.Println("Result from f1:", resultF1)
+}
