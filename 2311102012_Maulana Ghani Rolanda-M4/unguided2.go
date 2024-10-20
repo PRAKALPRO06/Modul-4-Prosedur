cfda120ade60b0b121fc1f072a36222c982b5b55
OlandMG <164113199+OlandMG@users.noreply.github.com>
2024-10-21 04:53:02 +0700

    Add files via upload

---
+++ 2311102012_Maulana Ghani Rolanda-M4/unguided2.go
@@ -0,0 +1,44 @@
+package main
+import "fmt"
+func hitungSkor(jumlah_soal_selesai * int, skor * int) bool {
+	const jumlah_soal int = 8
+	var jumlah_soal_selesai_lokal int = jumlah_soal
+	var t = [jumlah_soal] int {}
+	skor_lokal := 0
+	for i := 0; i<jumlah_soal; i++ {
+		fmt.Scan(&t[i])
+		if t[i] != 301 {
+			skor_lokal += t[i]
+		} else {
+			jumlah_soal_selesai_lokal --
+		}
+	}
+	if jumlah_soal_selesai_lokal > *jumlah_soal_selesai {
+		*skor = skor_lokal
+		*jumlah_soal_selesai = jumlah_soal_selesai_lokal
+		return true
+	} else if jumlah_soal_selesai_lokal == *jumlah_soal_selesai {
+		if skor_lokal > *skor {
+			*skor = skor_lokal
+			*jumlah_soal_selesai = jumlah_soal_selesai_lokal
+			return true
+		}
+	}
+	return false
+}
+
+func main() {
+	var jumlah_soal_selesai, skor int
+	var nama_pemenang, nama string
+	for {
+		fmt.Scan(&nama)
+		if nama == "Selesai" { break }
+		if hitungSkor(&jumlah_soal_selesai, &skor) {
+			nama_pemenang = nama
+		}
+	}
+	fmt.Print("\n")
+	fmt.Println(nama_pemenang, jumlah_soal_selesai, skor)
+}
+
+//Maulana Ghani Rolanda 2311102012
---
+++ 2311102012_Maulana Ghani Rolanda-M4/unguided3.go
@@ -0,0 +1,31 @@
+package main 
+import "fmt"
+func cetakDeret(n int) {
+	fmt.Print(n, " ")
+	prev := n
+	current := n
+	for i:=0; i<n; i++ {
+		if prev % 2 == 0 {
+			current = prev / 2
+			
+		} else {
+			current = 3*prev + 1
+		}
+		prev = current
+		fmt.Print(current, " ")
+		if current == 1 { break }
+	}
+}
+
+func main() {
+	var n int
+	fmt.Print("n: ")
+	fmt.Scan(&n)
+	if n > 1000000 {
+		fmt.Println("Input tidak boleh melebihi 1000000")
+		return
+	}
+	cetakDeret(n)
+}
+
+//Maulana Ghani Rolanda 2311102012
